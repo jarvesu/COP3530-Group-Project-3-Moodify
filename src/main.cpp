393b2ca33aa223fc107c116a5e9866e1907e211b
jarvesu <>
2021-04-19 14:38:04 -0400

    Reworked load-songs and main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "song.h"
 #include "retrieve_data.h"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <map>
 
+using namespace std;
 int main()
 {
-    std::vector<Song> songs;
+    
     loadSongs(songs);
 
     songs.at(0).getArtists();
