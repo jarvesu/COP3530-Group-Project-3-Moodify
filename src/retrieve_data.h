311be0c6fc71609e88f729ae97b7d77600e65cf3
Giovanni <giocornejo424@gmail.com>
2021-04-20 14:50:26 -0400

    Removed unnecessary data points

--- src/retrieve_data.h
+++ src/retrieve_data.h
@@ -8,22 +8,59 @@ using namespace std;
 //Loads song from a row
 Song loadSong(std::ifstream& file, std::string& data)
 {
-    //19 Elements from CSV Row
-    std::string elements[19];
-    for(int i = 0; i < 19; i++)
-    {
-        if(i == 18) //If final element in row
-        {
-            file >> data;
-            elements[i] = data;
-            continue;
-        }
+    //19 Elements from CSV Row, Only Need 
+    std::string elements[13];
 
-        getline(file, data, ',');
-        elements[i] = data;
-    }
+    getline(file, data, ',');
+    elements[0] = data; //Acousticness
 
-    return Song(elements[0], elements[1], elements[2], elements[3], elements[4], elements[5], elements[6], elements[7], elements[8], elements[9], elements[10], elements[11], elements[12], elements[13], elements[14], elements[15], elements[16], elements[17], elements[18]);
+    getline(file, data, ',');
+    elements[1] = data; //Artists
+
+    getline(file, data, ',');
+    elements[2] = data; //Danceability
+
+    getline(file, data, ',');
+    elements[3] = data; //Duration
+
+    getline(file, data, ',');
+    elements[4] = data; //Energy
+
+    getline(file, data, ',');
+    elements[5] = data; //Explicit
+
+    getline(file,data, ',');
+    elements[6] = data; //ID
+
+    getline(file, data, ',');
+    elements[7] = data; //Instrumentalness
+
+    getline(file, data, ','); //Skipping the Key
+
+    getline(file, data, ',');
+    elements[8] = data; //Liveness
+
+    getline(file, data, ','); //Skipping the Loudness
+    getline(file, data, ','); //Skipping the Mode
+
+    getline(file, data, ',');
+    elements[9] = data; //Name
+
+    getline(file, data, ','); //Skipping the Popularity
+    getline(file, data, ','); //Skipping the Release Date
+
+    getline(file, data, ',');
+    elements[10] = data; //Speechiness
+
+    getline(file, data, ',');
+    elements[11] = data; //Tempo
+
+    getline(file, data, ',');
+    elements[12] = data; //Happiness
+
+    file >> data; //Skipping the year
+
+    return Song(elements[0], elements[1], elements[2], elements[3], elements[4], elements[5], elements[6], elements[7], elements[8], elements[9], elements[10], elements[11], elements[12]);
 }
 
 //Loads data from file
